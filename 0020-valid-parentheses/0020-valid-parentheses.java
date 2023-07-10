91ea2366ae9fd1c6b85a2f0b0b15abebc3471749
Swapnil Uday Gaikwad <swapnil1329g@gmail.com>
2023-07-10 22:44:50 +0530

    Time: 3 ms (16.11%), Space: 40.6 MB (56.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,39 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
 
+        for(char ch : s.toCharArray()){
             if(ch=='('||ch=='['||ch=='{'){
                 stack.push(ch);
             }
-
-            if(stack.isEmpty()){
-                return false;
-            }
-
-            char check;
-            switch(ch){
-                case ')':
-                    check = stack.pop();
-                    if(check=='{'||check=='['){
+            else{
+                    if(stack.isEmpty()){
                         return false;
                     }
-                    break;
-                case ']':
-                    check = stack.pop();
-                    if(check=='{'||check=='('){
+                    if(ch==')' && stack.peek()=='('){
+                        stack.pop();
+                    }
+                    else if(ch==']' && stack.peek()=='['){
+                        stack.pop();
+                    }
+                    else if(ch=='}' && stack.peek()=='{'){
+                        stack.pop();
+                    }
+                    else{
                         return false;
                     }
-                    break;
-                case '}':
-                    check = stack.pop();
-                    if(check=='('||check=='['){
-                        return false;
-                    }
-                    break;
             }
         }
-        return stack.empty();
+        return stack.isEmpty();
     }
 }
