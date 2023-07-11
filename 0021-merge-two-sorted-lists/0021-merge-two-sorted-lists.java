6cfe6913326c0d33fbeb086c9d3c3865ad868af0
Swapnil Uday Gaikwad <swapnil1329g@gmail.com>
2023-07-11 23:58:33 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (99.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,19 +10,25 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
         if(list1 == null){
-            return list2;
-        }
-        if(list2 == null){
-            return list1;
-        }
-        if(list1.val < list2.val){
-            list1.next = mergeTwoLists(list1.next,list2);
-            return list1;
+            tail.next = list2;
         }
         else{
-            list2.next = mergeTwoLists(list1,list2.next);
-            return list2;
+            tail.next = list1;
         }
+        return dummy.next;
     }
 }
