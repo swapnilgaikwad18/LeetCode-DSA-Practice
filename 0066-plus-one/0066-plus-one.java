a41b8ba58355bb4e1e024f18c41d673051393a7f
Swapnil Uday Gaikwad <swapnil1329g@gmail.com>
2023-07-14 21:08:24 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (23.31%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int l=digits.length;
+        for(int i = l-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+
+        digits = new int[l+1];
+        digits[0] = 1;
+        return digits;
+    }
+}
