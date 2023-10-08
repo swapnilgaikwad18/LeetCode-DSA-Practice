58a9f85da9a864ad11d50b6b304e2d99b19fc9f4
Swapnil Uday Gaikwad <swapnil1329g@gmail.com>
2023-10-08 11:46:29 +0530

    Added solution - LeetHub

--- Rotating an Array - GFG/rotating-an-array.java
+++ Rotating an Array - GFG/rotating-an-array.java
@@ -52,8 +52,10 @@ class Solution {
             arr[i-d] = arr[i];
         }
         
+        int j=0;
         for(int i=n-d;i<n;i++){
-            arr[i] = temp[i-(n-d)];
+            arr[i] = temp[j];
+            j++;
         }
     }
 }
